8ee458edbf3bdc30fef80205153b551e47c7768a
hhcgit <hhc50331994@gmail.com>
2019-03-25 15:18:33 -0700

    question2

--- main.js
+++ main.js
@@ -5,3 +5,15 @@ function jediName(firstName,lastName){
     let jedi = last+first
     return jedi;
 }
+function beyond(num){
+
+    if (num>0){
+        return console.log("To infinity And beyond");
+    }
+    else if (num<0){
+        console.log("To negative infinity And beyond");
+    }
+    else if(num===0){
+        console.log("staying home");
+    }
+}
