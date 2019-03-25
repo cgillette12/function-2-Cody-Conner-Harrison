40d0814813fd843822648dabad6f7c456d828892
Cody Gillette <cgillette1312@gmail.com>
2019-03-25 15:55:57 -0700

    decode

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ function jediName(firstName,lastName){
     let first = firstName.slice(0,1);
     let last = lastName.slice(0,3);
 
-    let jedi = last+first
+    let jedi = last+first;
     return jedi;
 }
 function beyond(num){
@@ -17,3 +17,24 @@ function beyond(num){
         console.log("staying home");
     }
 }
+
+function decode(word){
+    let outPut = '';
+    let breakApart = word.split(' ');
+    for(let i = 0; i < breakApart.length; i++){
+      if(breakApart[i].charAt(0) === 'a'){
+        outPut +=breakApart[i].charAt(1);
+      }else if(breakApart[i].charAt(0) === 'b'){
+        outPut +=breakApart[i].charAt(2);
+      }else if(breakApart[i].charAt(0) === 'c'){
+        outPut +=breakApart[i].charAt(3);
+      }else if(breakApart[i].charAt(0) === 'd'){
+        outPut +=breakApart[i].charAt(4);
+      }else{
+        outPut+= " ";
+      }
+    }
+   console.log(outPut);
+
+}
+decode('craft block argon meter bells brown croon droop');
